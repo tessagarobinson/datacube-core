738e75685c12fe49faf4a67d37ca854ebacddcd5
Damien Ayers <damien@omad.net>
2019-08-29 15:18:55 +1000

    Fix docker stuff

--- docker/run-tests-conda.sh
+++ docker/run-tests-conda.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash -l
+#!/bin/bash -l
+
+set -euxo pipefile
 
 conda activate odc
-conda install -y -c conda-forge compliance-checker hypothesis mock pytest pytest-cov pytest-timeout moto celery sharedarray redis-py pathos
 pip install pytest-httpserver zstandard
-pytest -m "not netcdf and not s3aio" -r a --cov datacube --durations=5 tests integration_tests --odc-config-file odcintegration-docker.conf
+pytest -r a --cov datacube --durations=5 tests integration_tests --odc-config-file odcintegration-docker.conf
