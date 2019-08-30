93f4935a1ce1cb0167ed1a2d4627e45ef1179b2d
Damien Ayers <damien@omad.net>
2019-08-30 15:05:21 +1000

    Docker scripts shuffle

--- docker/run-tests-conda.sh
+++ docker/run-tests-conda.sh
@@ -3,6 +3,5 @@
 #set -euxo pipefail # Can't use this, 'conda activate' has unbound variables
 set -x
 
-conda activate odc
 pip install pytest-httpserver zstandard
-pytest -r a --cov datacube --durations=5 tests integration_tests --odc-config-file odcintegration-docker.conf
+pytest tests integration_tests --odc-config-file odcintegration-docker.conf
